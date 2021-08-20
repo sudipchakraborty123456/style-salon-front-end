49ad2a748f35731df74fa20a9f6d8954272c08bf
SUDIP CHAKRABORTY <sudipchakraborty47@gmail.com>
2021-08-20 12:24:49 +0530

    jhsgdf

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,5 @@
 const DEVELOPMENT_URL = "http://localhost:5420/api";
-const PRODUCTION_URL = " https://style-salon-backend.herokuapp.com"; //got from heroku
+const PRODUCTION_URL = " https://style-salon-backend.herokuapp.com/api"; //got from heroku
 exports.API_URL =  PRODUCTION_URL;
 
 
